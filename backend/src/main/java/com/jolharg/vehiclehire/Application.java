4115a09654f74fe7fac54a69817f8ec280d63088
Dan Dart <git@dandart.co.uk>
2020-11-06 02:32:51 +0000

    compiles now

--- backend/src/main/java/com/jolharg/vehiclehire/Application.java
+++ backend/src/main/java/com/jolharg/vehiclehire/Application.java
@@ -12,7 +12,7 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Example.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
