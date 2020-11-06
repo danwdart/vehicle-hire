ed956ec2f3694c6a5e1aaed46cec5e46de8bc7df
Dan Dart <git@dandart.co.uk>
2020-11-06 02:23:57 +0000

    Fixes to java

--- backend/src/main/java/com/jolharg/vehiclehire/Application.java
+++ backend/src/main/java/com/jolharg/vehiclehire/Application.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableAutoConfiguration
-public class Example {
+public class Application {
 
     @RequestMapping("/")
     String home() {
